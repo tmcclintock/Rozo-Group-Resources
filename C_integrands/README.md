50248246fb668b9a68068ec00602ade814c739e6
Tom McClintock <tmcclintock89@gmail.com>
2017-05-27 07:51:20 -0700

    Working on the C_integrands example.

---
+++ C_integrands/README.md
@@ -0,0 +1,1 @@
+This subdirectory is used to demonstrate how we can write integrands in C in order to expedite the computation of an integral.
---
+++ C_integrands/integral_examples.py
@@ -0,0 +1,9 @@
+"""
+Here we write how to do some integral numerically, and
+compare the computational speed.
+"""
+import numpy as np
+from scipy import optimize as op
+
+def integrand(x, R):
+    return 0
