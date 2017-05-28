0095ae8a9118a0a564027cf211a14d79c88a4e57
Tom McClintock <tmcclintock89@gmail.com>
2017-05-28 07:52:08 -0700

    Added an integral and had it run for a long time.

--- C_integrands/integral_examples.py
+++ C_integrands/integral_examples.py
@@ -1,9 +1,23 @@
 """
 Here we write how to do some integral numerically, and
 compare the computational speed.
+
+The integral is \int_\pi^{2\pi} cos(x)/x^3 dx \approx -0.0152115
+
 """
 import numpy as np
-from scipy import optimize as op
+import time
+from scipy.integrate import quad
 
-def integrand(x, R):
-    return 0
+def integrand(x):
+    return np.cos(x)/x**3
+
+def do_integral():
+    return quad(integrand, np.pi, 2*np.pi)
+
+if __name__ == "__main__":
+    start = time.time()
+    for i in xrange(100000):
+        answer = do_integral()
+    end = time.time()
+    print end - start
