5f84fa87693d642206b332e67b090c5d923cc836
Tom McClintock <tmcclintock89@gmail.com>
2017-05-29 09:03:23 -0700

    Added an example that includes arguments to the c function.

--- C_integrands/integrand.c
+++ C_integrands/integrand.c
@@ -1,5 +1,12 @@
 #include<math.h>
 
-double integrand(int i, double x){
+double integrand(int n, double x){
   return cos(x)/(x*x*x);
 }  
+
+double integrand2(int n, double args[n]){
+  double x = args[0];
+  double a = args[1];
+  double b = args[2];
+  return cos(a*x)/pow(x,b);
+}
