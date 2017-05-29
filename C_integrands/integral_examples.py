629a5566034d7930a08c8341f3b4cd25b6c9f982
Tom McClintock <tmcclintock89@gmail.com>
2017-05-29 07:35:37 -0700

    Got the example to work. Using c_types improves the runtime by greater than a factor of 2.

--- C_integrands/integral_examples.py
+++ C_integrands/integral_examples.py
@@ -8,6 +8,11 @@ The integral is \int_\pi^{2\pi} cos(x)/x^3 dx \approx -0.0152115
 import numpy as np
 import time
 from scipy.integrate import quad
+import ctypes
+lib = ctypes.cdll.LoadLibrary("integrand.so")
+c_integrand = lib.integrand
+c_integrand.argtypes = (ctypes.c_int, ctypes.c_double)
+c_integrand.restype  = ctypes.c_double
 
 def integrand(x):
     return np.cos(x)/x**3
@@ -15,9 +20,20 @@ def integrand(x):
 def do_integral():
     return quad(integrand, np.pi, 2*np.pi)
 
+def do_c_integral():
+    return quad(c_integrand, np.pi, 2*np.pi)
+
 if __name__ == "__main__":
+    print "Python version:", do_integral()
+    print "ctypes version:", do_c_integral()
+
     start = time.time()
     for i in xrange(100000):
         answer = do_integral()
     end = time.time()
-    print end - start
+    print end - start, "For python version"
+    start = time.time()
+    for i in xrange(100000):
+        answer = do_c_integral()
+    end = time.time()
+    print end - start, "For c_types version"
